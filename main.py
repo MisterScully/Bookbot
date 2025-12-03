f9f3b0986549d7c470e32260269b2bc4f14444a8
MisterScully <Misterscully920@gmail.com>
2025-12-03 14:25:48 -0500

    Updated main.py to read Frankenstein.txt

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(txt):
+    with open(txt) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    contents = get_book_text("books/frankenstein.txt")
+    print(contents)
+
+main()
